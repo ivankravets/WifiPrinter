b6d03c7e1854e71d69250d5b2f1d8c08b04b9af1
Bert Melis <bertmelis@users.noreply.github.com>
2018-02-24 23:58:37 +0100

    :boom: first commit

--- README.md
+++ README.md
@@ -1,23 +1,8 @@
-# Update --> Redesign!!
-I'm not maintaining the current code as I'm working on a completely new version.
-The new version will be totally different:
-- use the async webserver from @me-no-dev
-- display printed messages via websockets on a status page
-- incorporate an upload form for firmware updates
-- display basis stats like uptime, free heap, firmware version etc...
-This will be more close to my personal needs.
-
-As this was only a slim version of the original version of @JoaoLopesF, which is still actively maintained, you can switch over to that version.
-I will be changing the codebase completely in the coming weeks.
-
 # WifiPrinter
 [![Build Status](https://travis-ci.org/bertmelis/WifiPrinter.svg?branch=master)](https://travis-ci.org/bertmelis/WifiPrinter)
 
-Arduino library for ESP8266 to print debug messages on a telnet-like server.
+Arduino library for ESP8266 and ESP32 to print on a device-hosted webpage via websockets.
 
-Adds the ability to use a telnet-like server to print debug messages on when your Serial0 interface is occupied by something else or when you cannot physically reach your device.
+The webpage also displays some basic statistics and features an upload form for firmware updates.
 
-## Credits go to
-- (C) 2015 Rudolf Reuter http://www.rudiswiki.de/wiki9 --> original work
-- (C) 2016 JoaoLopesF https://github.com/JoaoLopesF --> RemoteDebug library
-- (C) 2017 Bert Melis https://github.com/bertmelis --> stripped down version
+Warning: the webserver is completely unprotected. Do not use in public environments!
--- src/WifiPrinter.cpp
+++ src/WifiPrinter.cpp
@@ -1,194 +1,154 @@
+/* copyright 2016 Bert Melis
+   License: MIT
+*/
+
 #include "WifiPrinter.h"
 
-
-WifiPrinter::WifiPrinter():
-  _wifiServer(23),
-  _buffer(""),
-  _connected(false),
-  _resetCommandEnabled(false),
-  _command(""),
-  _newLine(true),
-  _showTime(false)
-  {
-    _buffer.reserve(200);
-  }
-WifiPrinter::WifiPrinter(uint16_t port):
-  _wifiServer(port),
-  _buffer(""),
-  _connected(false),
-  _resetCommandEnabled(false),
-  _command(""),
-  _newLine(true),
-  _showTime(false)
-  {
-    _buffer.reserve(200);
-  }
-
+WifiPrinter::WifiPrinter() :
+  _server(80),
+  _ws("/ws"),
+  _flagForReboot(false),
+  _rebootCb(nullptr),
+  _fwVersion{"\0"} {}
 
 void WifiPrinter::begin() {
-  _wifiServer.begin();
-  _wifiServer.setNoDelay(true);
-  while(_wifiClient.available())
-    _wifiClient.read(); //clear input buffer
+  _ws.onEvent(std::bind(&WifiPrinter::_onEvent, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5, std::placeholders::_6));
+  _server.addHandler(&_ws);
+  _server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/html", index_html);
+  });
+  _server.on("/w3.css", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/css", w3_css);
+  });
+  _server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest *request) {
+    AsyncWebServerResponse *response = request->beginResponse_P(200, "image/x-icon", favicon_ico_gz, favicon_ico_gz_len);
+    response->addHeader("Content-Encoding", "gzip");
+    request->send(response);
+  });
+  _server.on("/update", HTTP_POST, std::bind(&WifiPrinter::_handleUpdateRDY, this, std::placeholders::_1),
+                                   std::bind(&WifiPrinter::_handleUpdate, this, std::placeholders::_1,
+                                                                                std::placeholders::_2,
+                                                                                std::placeholders::_3,
+                                                                                std::placeholders::_4,
+                                                                                std::placeholders::_5,
+                                                                                std::placeholders::_6));
+  _server.onNotFound(std::bind(&WifiPrinter::_onRequest, this, std::placeholders::_1));
+  _timer.attach(statsFreq, &WifiPrinter::_timerCb, this);
+  _server.begin();
 }
-
-
-void WifiPrinter::stop() {
-  if (_wifiClient && _wifiClient.connected()) _wifiClient.stop();
-  _wifiServer.stop();
-}
-
 
 void WifiPrinter::loop() {
-  //Handle connections
-  if (_wifiServer.hasClient()) {
-    if (!_wifiClient || !_wifiClient.connected()) {
-      if (_wifiClient) _wifiClient.stop(); //stop second client, only 1 supported
-      _wifiClient = _wifiServer.available();
-      _wifiClient.flush(); //start with clean sheet
-      _showInfo();
-      //empty input buffer
-      while(_wifiClient.available()) {
-        _wifiClient.read();
-      }
-    }
+  static uint32_t lastMillis = 0;
+  if (millis() - lastMillis <= 10 * 1000UL) {
+    lastMillis = millis();
+    // send stats
   }
-  //Loop through activities
-  _connected = (_wifiClient && _wifiClient.connected());
-  if (_connected) {
-    //check if commands has been sent
-    while(_wifiClient.available()) {
-      char character = _wifiClient.read();
-      if (character == '\n' || character == '\r') {
-        //Commands end with <newline> --> handle command
-        if (_command.length() > 0) {
-          _handle();
-        }
-        _command = ""; // Init for next command
-      } else if (isPrintable(character)) {
-        // Concat
-        _command.concat(character);
-      }
+  if (_flagForReboot) {
+    if (_rebootCb)
+      _rebootCb();
+    _ws.textAll("Rebooting...\n");
+    _ws.closeAll();
+    uint32_t timer = 0;
+    while (timer < 1000) {  // delay some time for async tasks to complete before restarting eg. disconnecting mqtt
+      yield();
+      ++timer;
     }
+    ESP.restart();
   }
 }
 
-
-// Allow ESP reset over telnet client
-void WifiPrinter::enableReset(boolean enable){
-    _resetCommandEnabled = enable;
+void WifiPrinter::_handleUpdateRDY(AsyncWebServerRequest *request) {
+  _flagForReboot = !Update.hasError();
+  AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", _flagForReboot?"OK":"FAIL");
+  response->addHeader("Connection", "close");
+  request->send(response);
 }
 
-
-boolean WifiPrinter::isActive() {
-    return _connected;
-}
-
-
-size_t WifiPrinter::write(uint8_t character) {
-  // New line writted before ?
-  if (_newLine) {
-    String show = "";
-    // Show time in millis
-    if (_showTime) {
-      show.concat ("(t: ");
-      show.concat (millis());
-      show.concat ("ms)");
-      if (_connected) {  // send data to Client
-        _buffer = show;
-      }
+void WifiPrinter::_handleUpdate(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
+  if (!index) {
+    Serial.printf("Update Start: %s\n", filename.c_str());
+#if defined ESP8266
+    Update.runAsync(true);
+    size_t size = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
+#elif defined ARDUINO_ARCH_ESP32
+    size_t size = UPDATE_SIZE_UNKNOWN;
+#endif
+    if (!Update.begin(size)) {
+      // Update.printError(Serial);
     }
-    _newLine = false;
   }
-
-  // Print ?
-  boolean doPrint = false;
-
-  // New line ?
-  if (character == '\n') {
-    _newLine = true;
-    doPrint = true;
-  } else if (_buffer.length() == 100) { // Limit of buffer
-    doPrint = true;
+  if (!Update.hasError()) {
+    if (Update.write(data, len) != len) {
+      // Update.printError(Serial);
+    }
   }
-
-  // Write to telnet Buffered
-  _buffer.concat((char)character);
-
-  // Send the characters buffered by print.h
-  if (doPrint) { // Print the buffer
-    // Write to telnet Buffered
-    if (_connected) {  // send data to Client
-      _wifiClient.print(_buffer);
+  if (final) {
+    if (Update.end(true)) {
+      // Serial.printf("Update Success: %uB\n", index+len);
+    } else {
+      // Update.printError(Serial);
     }
-    // Empty the buffer
-    _buffer = "";
-    return 1;
-  } else return 1;
+  }
 }
 
-////// Private
-
-// Show help of commands
-void WifiPrinter::_showInfo() {
-    // Show the initial message
-    String help = "";
-    help.concat(F("* ESP8266 WifiPrinter\r\n"));
-    help.concat(F("* Commands:\r\n"));
-    help.concat(F("   ? or help -> display these help of commands\r\n"));
-    help.concat(F("   t -> display timestamps\r\n"));
-    help.concat(F("   m -> display memory available\r\n"));
-//    help.concat(F("   i -> display crash info\r\n"));
-//    help.concat(F("   c -> clear crash info\r\n"));
-    help.concat(F("   q -> quit (close this connection)\r\n"));
-    if (_resetCommandEnabled) {
-        help.concat(F("   reset -> reset the ESP8266\r\n"));
+void WifiPrinter::_onEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len) {
+  if (type == WS_EVT_CONNECT) {
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["sdkVersion"] = ESP.getSdkVersion();
+#ifdef ARDUINO_ARCH_ESP32
+    json["coreVersion"] = "n/a on esp32";
+#else
+    json["coreVersion"] = ESP.getCoreVersion();
+#endif
+    json["fwVersion"] = _fwVersion;
+    json["wpVersion"] = _wpVersion;
+    size_t len = json.measureLength();
+    AsyncWebSocketMessageBuffer* wsBuffer = _ws.makeBuffer(len);
+    if (wsBuffer) {
+      json.printTo(reinterpret_cast<char*>(wsBuffer->get()), len + 1);
+      client->text(wsBuffer);
     }
-    help.concat(F("* Please type the command and press enter to execute. (? or h for this help)\r\n\r\n"));
-    _wifiClient.print(help);
+  }
 }
 
+void WifiPrinter::_onRequest(AsyncWebServerRequest *request) {
+  // Handle Unknown Request
+  request->send(404);
+}
 
-// Process user command over telnet
-void WifiPrinter::_handle() {
-    _wifiClient.print(F("Command received: "));
-    _wifiClient.println(_command);
-    String options = "";
-    uint8_t pos = _command.indexOf(" ");
-    if (pos > 0) {
-        options = _command.substring (pos+1);
-    }
-    // Process the command
-    if (_command == "h" || _command == "?") {
-        // Show help
-        _showInfo();
-    } else if (_command == "q") {
-        // Quit
-        _wifiClient.println(F("Closing telnet connection ..."));
-        _wifiClient.stop();
-    } else if (_command == "m") {
-        _wifiClient.print(F("* Free Heap RAM: "));
-        _wifiClient.println(ESP.getFreeHeap());
-/*    } else if (_command == "i") {
-      //display crash info
-      //_wifiClient.println(F("* Display crash info"));
-      SaveCrash.print(_wifiClient);
-    } else if (_command == "c") {
-      //clear crash info
-      SaveCrash.clear();
-      _wifiClient.println(F("* Crash info cleared.")); */
-    }  else if (_command == "t") {
-        // Show time
-        _showTime = !_showTime;
-        _wifiClient.printf("* Show time: %s\r\n", (_showTime)?"On":"Off");
-    }  else if (_command == "reset" && _resetCommandEnabled) {
-        _wifiClient.println(F("* Reset ..."));
-        _wifiClient.println(F("* Closing telnet connection ..."));
-        _wifiClient.println(F("* Resetting the ESP8266 ..."));
-        _wifiClient.stop();
-        _wifiServer.stop();
-        delay (500);
-        // Reset
-        ESP.reset();
-    }
+size_t WifiPrinter::write(uint8_t str) {
+  return write(&str, 1);
+}
+
+size_t WifiPrinter::write(const uint8_t* buffer, size_t size) {
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& json = jsonBuffer.createObject();
+  json["message"] = buffer;
+  size_t len = json.measureLength();
+  AsyncWebSocketMessageBuffer* wsBuffer = _ws.makeBuffer(len);
+  if (wsBuffer) {
+    json.printTo(reinterpret_cast<char*>(wsBuffer->get()), len + 1);
+    _ws.textAll(wsBuffer);
+  }
+  return len;
+}
+
+void WifiPrinter::_timerCb(WifiPrinter* wp) {
+  if (wp) {
+    wp->_updateStats();
+  }
+}
+
+void WifiPrinter::_updateStats() {
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& json = jsonBuffer.createObject();
+  json["freeHeap"] = ESP.getFreeHeap();
+  json["uptime"] = millis();
+  size_t len = json.measureLength();
+  AsyncWebSocketMessageBuffer* wsBuffer = _ws.makeBuffer(len);
+  if (wsBuffer) {
+    json.printTo(reinterpret_cast<char*>(wsBuffer->get()), len + 1);
+    _ws.textAll(wsBuffer);
+  }
 }
